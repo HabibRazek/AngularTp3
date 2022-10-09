07af81bf2a50f99d7b4e78b2d605097d17d5ec08
HabibRazek <habibrazeg32@gmail.com>
2022-10-09 21:21:18 +0100

    angularTp3

---
+++ angular-tp3/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+
+@NgModule({
+  declarations: [AppComponent, ProduitsComponent, AddProduitComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
